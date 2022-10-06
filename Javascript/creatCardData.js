a82e14820b2e69a5e2adffc597c869d13324415f
WAQUET Valentin <waquetv@gmail.com>
2022-10-06 18:57:40 +0200

    add Data array

--- Javascript/creatCardData.js
+++ Javascript/creatCardData.js
@@ -1,5 +1,146 @@
 const cardWrapperData = document.querySelector(".slider-containerData");
 
+const data = [
+    {
+        slide: "slider-data",
+        control: "btnControlData1",
+        picture: "./assets/DATA/Sans-titre-1.jpg",
+        firstName: "Grégory",
+        lastName: "JAYET",
+        github: "https://github.com/Greg59830",
+        linkedin: "https://www.linkedin.com/in/grégory-jayet/",
+        portfolio: "#",
+        personalPres: "Je suis l’heureux papa de 2 filles qui me donnent chaque jour l’envie d’apprendre et de m'améliorer. ",
+        professionnalPres: "on sens de la relation client, et mon adaptabilité peuvent être un avantage pour mon nouveau projet: Analyste de données Devenir. Je serais ravi de partager tout cela lors d’un stage où alternance à partir de février 2023.",
+    },
+    {
+        slide: "slider-data",
+
+        control: "btnControlData2",
+        picture: "./assets/DATA/Sans-titre-2.jpg",
+        firstName: "TOUZANI",
+        lastName: "Asma",
+        github: "https://github.com/Atouzani7",
+        linkedin: "#",
+        portfolio: "#",
+        personalPres: "Lorem ipsum",
+        professionnalPres:
+            "En reconversion professionnel, pour devenir développeuse web",
+    },
+    {
+        slide: "slider-data",
+
+        control: "btnControlData3",
+        picture: "./assets/DATA/Sans-titre-3.jpg",
+        firstName: "JUTEAU",
+        lastName: "Charly",
+        github: "#",
+        linkedin: "#",
+        portfolio: "#",
+        personalPres: "Lorem ipsum",
+        professionnalPres: "lorem ipsum",
+    },
+    {
+        slide: "slider-data",
+
+        control: "btnControlData4",
+        picture: "./assets/DATA/Sans-titre-4.jpg",
+        firstName: "BARTIER",
+        lastName: "Florian",
+        github: "#",
+        linkedin: "#",
+        portfolio: "#",
+        personalPres: "Lorem ipsum",
+        professionnalPres: "lorem ipsum",
+    },
+    {
+        slide: "slider-data",
+
+        control: "btnControlData5",
+        picture: "./assets/DATA/Sans-titre-5.jpg",
+        firstName: "BOUCHEZ",
+        lastName: "Florian",
+        github: "https://github.com/Fbouchezpro",
+        linkedin: "ttps://www.linkedin.com/in/florian-bouchez-18521b23b/",
+        portfolio: "#",
+        personalPres: "Student Web Developer @ Wild Code school",
+        professionnalPres:
+            "Currently in search of a stage in IT enterprise , and in future create a web agency.",
+    },
+    {
+        slide: "slider-data",
+
+        control: "btnControlData6",
+        picture: "./assets/DATA/Sans-titre-6.jpg",
+        firstName: "WAQUET",
+        lastName: "Valentin",
+        github: "#",
+        linkedin: "#",
+        portfolio: "#",
+        personalPres: "Lorem ipsum",
+        professionnalPres: "lorem ipsum",
+    },
+    {
+        slide: "slider-data",
+
+        control: "btnControlData7",
+        picture: "./assets/DATA/Sans-titre-7.jpg",
+        firstName: "CARTIAUX",
+        lastName: "Gérald",
+        github: "https://github.com/cartige",
+        linkedin: "https://www.linkedin.com/in/gerald-cartiaux-a97254251",
+        portfolio: "#",
+        personalPres:
+            "Passioné des jeux vidéos, guitariste amateur. Soft skills : anglais niveau correct, àl'écoute, attentif. Language informatique : C, JS, CSS, HTML, Java, Vue.",
+
+        professionnalPres: "Trouver un job dans le dévelopement web.",
+    },
+    {
+        slide: "slider-data",
+
+        control: "btnControlData8",
+        picture: "./assets/DATA/Sans-titre-8.jpg",
+        firstName: "SWIERGIEL",
+        lastName: "Hervé",
+        github: "https://github.com/hswiergiel",
+        linkedin: "https://www.linkedin.com/in/herv%C3%A9-swiergiel-63882352/",
+        portfolio: "#",
+        personalPres: "Chanteur choriste, randonneur et nouvellement développeur",
+        professionnalPres:
+            "Fonctionnaire territorial pendant 7 ans, je me reconvertis dans le domaine dudéveloppement web et suis à la recherche d'une première expérience professionnelle",
+    },
+    {
+        slide: "slider-data",
+
+        control: "btnControlData9",
+        picture: "./assets/DATA/Sans-titre-9.jpg",
+        firstName: "GUEGUEN",
+        lastName: "Kévin",
+        github: "https://github.com/guenk",
+        linkedin: "https://www.linkedin.com/in/GueguenK",
+        portfolio: "#",
+        personalPres:
+            "Rigoureux, autonome et organisé, je suis actuellement en formation de développeur web à la Wild Code School.",
+        professionnalPres:
+            "Je souhaite rejoindre une entreprise dans le cadre d'un stage ou d'une alternance dés janvier 2022.",
+    },
+    {
+        slide: "slider-data",
+
+        control: "btnControlJ10",
+        picture: "./assets/DATA/Sans-titre-10.jpg",
+        firstName: "FOULONT",
+        lastName: "Alexis",
+        github: "https://github.com/AlexisFLT",
+        linkedin: "https://www.linkedin.com/in/alexis-foulont",
+        portfolio: "#",
+        personalPres:
+            "Esprit d'équipe, capacité d'adaptation et autonomie sont des softs skills que  j'aimerais mettre à profit pour mon futur métier. Une citation d'une personne anonyme me résume parfaitement : “A good team needs three things: the right people",
+        professionnalPres:
+            "Après 6 ans dans la Petite Enfance, j'ai décidé de me reconvertir dans l'informatique où je me suis découvert une passion. J'ai désormai le souhait d'en faire mon métier. Je suis actuellement en formation Developpeur Web à la Wild Code School de Lille etje cherche un contrat de professionnalisation pour mars 2023.",
+    },
+];
+
 function createCardData(
     slide,
     control,
@@ -131,17 +272,17 @@ function createCardData(
     cardBack.appendChild(imgProfil1);
 }
 
-for (let i = 0; i < php.length; i++) {
+for (let i = 0; i < data.length; i++) {
     createCardData(
-        php[i].slide,
-        php[i].control,
-        php[i].picture,
-        php[i].firstName,
-        php[i].lastName,
-        php[i].github,
-        php[i].linkedin,
-        php[i].portfolio,
-        php[i].personalPres,
-        php[i].professionnalPres
+        data[i].slide,
+        data[i].control,
+        data[i].picture,
+        data[i].firstName,
+        data[i].lastName,
+        data[i].github,
+        data[i].linkedin,
+        data[i].portfolio,
+        data[i].personalPres,
+        data[i].professionnalPres
     );
 }
